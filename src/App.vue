6819f8d9999c6ed7429cf58c42e7a80cbe753d74
samantha <samanthapelkova1@gmail.com>
2023-10-16 15:28:20 +0200

    adding components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
-<div>
-
-</div>
+  <div>
+    <HeaderComponent/>
+    <OneRecipe/>
+  </div>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue'
+import OneRecipe from './components/OneRecipe.vue'
+
+export default {
+  components:{
+    HeaderComponent,
+    OneRecipe
+  },
+  setup(){
+
+  }
+
+}
 
 </script>
 
