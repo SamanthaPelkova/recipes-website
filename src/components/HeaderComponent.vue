1f9f974e729bded248e0939cf84cb7d2a90d10d6
samantha <samanthapelkova1@gmail.com>
2023-10-17 11:54:04 +0200

    adding styles

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="headers">
-    <AllRecipes/>
-    <AddRecipe/>
+    <a href="AllRecipes.vue" class="allRecipes"> <AllRecipes/></a>
+    <a href="AddRecipe.vue" class="addRecipe"><AddRecipe/></a>
   </div>
 </template>
 
@@ -18,5 +18,35 @@ export default {
 </script>
 
 <style>
+.headers{
+  background-color: white;
+  border-radius: 10px;
+}
+
+.headers{
+  display: flex;
+  align-items: center;
+  margin: 20px 50px 50px;
+}
+.addRecipe {
+  margin-left: 20px;
+  text-decoration: none;
+  color: wheat;
+  transition: color 0.3s;
+}
+.addRecipe:hover{
+  color: #b09e7d;
+}
+
+.allRecipes{
+  text-decoration: none;
+  color: wheat;
+  padding-left: 10px;
+  transition: color 0.3s;
+}
+
+.allRecipes:hover{
+  color: #b09e7d;
+}
 
 </style>
