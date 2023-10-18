d054a62d6cd3ed1fbc73062a89cebc6a16f59114
samantha <samanthapelkova1@gmail.com>
2023-10-18 15:04:44 +0200

    removing .ts

--- src/components/AllRecipes.vue
+++ src/components/AllRecipes.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="nameAllRecepies">
+  <div class="nameAllRecipes">
     <h2>All Recipes</h2>
     <p>fdfdmodmfodm</p>
     <p>fdfdmodmfodm</p>
@@ -14,13 +14,44 @@
 
 <script lang="ts">
 import { ref } from "vue";
-import {allRecipesInfo, Recipe} from "../allRecipes";
 
 export default {
   setup(){
-  const allRecipesText = ref<Recipe>(allRecipesInfo)
+    const allRecipesText = ref( [
+        {
+          name: 'Pasta Carbonara',
+          ingredients: ['Spaghetti', 'Eggs', 'Bacon', 'Parmesan Cheese'],
+          instructions: 'Cook pasta; Mix eggs, bacon, and cheese; Toss with pasta.',
+        },
+        {
+          name: 'Chicken Stir-Fry',
+          ingredients: ['Chicken', 'Vegetables', 'Soy Sauce', 'Rice'],
+          instructions: 'Stir-fry chicken and vegetables; Add soy sauce; Serve over rice.',
+        },
+        {
+          name: 'Vegetarian Pizza',
+          ingredients: ['Pizza dough', 'Tomato sauce', 'Cheese', 'Vegetables'],
+          instructions: 'Roll out dough; Spread sauce; Add cheese and vegetables; Bake in the oven.',
+        },
+        {
+          name: 'Chocolate Cake',
+          ingredients: ['Flour', 'Sugar', 'Cocoa powder', 'Eggs', 'Milk'],
+          instructions: 'Mix dry ingredients; Add wet ingredients; Bake in a preheated oven.',
+        },
+        {
+          name: 'Tomato Basil Salad',
+          ingredients: ['Tomatoes', 'Fresh basil', 'Olive oil', 'Salt', 'Pepper'],
+          instructions: 'Slice tomatoes; Chop fresh basil; Drizzle with olive oil; Season with salt and pepper.',
+        },
+        {
+          name: 'Grilled Salmon',
+          ingredients: ['Salmon fillets', 'Lemon', 'Garlic', 'Olive oil', 'Salt', 'Pepper'],
+          instructions: 'Marinate salmon; Grill until cooked; Squeeze lemon juice before serving.',
+        }
 
-    return{
+      ])
+
+    return {
       allRecipesText
     }
   }
@@ -28,19 +59,19 @@ export default {
 </script>
 
 <style>
-.nameAllRecepies{
+.nameAllRecipes{
   background-color: #b09e7d;
   border-radius: 10px;
   padding: 10px;
 }
 
-.nameAllRecepies h2{
+.nameAllRecipes h2{
   color: white;
   font-size: 30px;
   margin: 3px 7px;
 }
 
-.nameAllRecepies p{
+.nameAllRecipes p{
   color: white;
   margin-left: 25px;
 }
